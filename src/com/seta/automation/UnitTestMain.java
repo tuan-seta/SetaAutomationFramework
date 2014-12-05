916ade1a9749062768a86dad560126a1d8290b28
tuan-seta <tuan.trinh@setacinq.vn>
2014-12-06 04:42:54 +0700

    Update excel parser

--- src/com/seta/automation/UnitTestMain.java
+++ src/com/seta/automation/UnitTestMain.java
@@ -2,25 +2,39 @@ package com.seta.automation;
 
 import com.seta.automation.config.Config;
 import com.seta.automation.data.LoadableControl;
+import com.seta.automation.utils.ExcelHelper;
 
 public class UnitTestMain {
 
 	public static void main(String[] args) {
-		getConfig();
+//		getConfig();
+		parseExcelData();
 	}
 
-//	public static void loadProperty(){
-//		String fPath = "conf/default.properties";
-//		try {
-//			LoadableControl config = new LoadableControl(fPath);
-//			System.out.print(config.toString());
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
+	public static void loadProperty(){
+		String fPath = "conf/default.properties";
+		try {
+			LoadableControl config = new LoadableControl(fPath);
+			System.out.print(config.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 	private static void getConfig(){
 		Config config = Config.getInstance();
 		System.out.print(config.toString());
+	}
+	
+	private static void parseExcelData(){
+		String fPath = "input/data/LoginData.xlsx";
+		String sheetName = "LoginMultiTime";
+		try {
+			ExcelHelper parser = new ExcelHelper(fPath, sheetName);
+			System.out.print(parser.toString());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
--- src/com/seta/automation/utils/ExcelHelper.java
+++ src/com/seta/automation/utils/ExcelHelper.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -16,18 +17,22 @@ import com.seta.automation.data.LoadableData;
 public class ExcelHelper {
 	private static Sheet excelSheet;
 	private static Workbook excelBook;
+	private String fPath;
+	private String sheetName;
 	
-	private ArrayList<LoadableData> testcaseData;
+	private HashMap<Integer, LoadableData> testcaseData;
 	
 	// This method is to set the File path and to open the Excel file, Pass
 	// Excel Path and Sheetname as Arguments to this method
-	public void ExcelHelper(String fPath, String SheetName) throws Exception {
+	public ExcelHelper(String fPath, String sheetName) throws Exception {
+		this.fPath = fPath;
+		this.sheetName = sheetName;
 		try {
 			// Open the Excel file
 			FileInputStream ExcelFile = new FileInputStream(fPath);
 			// Access the required test data sheet
 			excelBook = WorkbookFactory.create(ExcelFile);
-			setExcelSheet(SheetName);
+			setExcelSheet(sheetName);
 			parseTestcaseData();
 			ExcelFile.close();
 		}catch (FileNotFoundException e) {
@@ -55,6 +60,9 @@ public class ExcelHelper {
 		excelBook.getCreationHelper().createFormulaEvaluator();
 	}
 
+	//check data sheet format
+	//initiate hashmap to load testcase data 
+	//return number of testcase in excel sheet
 	private int getNumOfTestcase(){
 		Row firstRow = excelSheet.getRow(0);
 		int count = 0;
@@ -72,6 +80,12 @@ public class ExcelHelper {
 				count ++;
 			} 
 		}
+		//Initiate hashmap parsing testcase data
+		testcaseData = new HashMap<Integer, LoadableData>(count);
+		for (int i=2; i< count + 2; i++){
+			LoadableData testcase = new LoadableData();
+			testcaseData.put(i, testcase);
+		}
 		return count;
 	}
 	
@@ -81,25 +95,45 @@ public class ExcelHelper {
 			return;
 		} 
 		
-		testcaseData = new ArrayList<LoadableData>(count);
 		Row fieldData;
 		for (int i = 1; i < excelSheet.getPhysicalNumberOfRows(); i++) {
 			fieldData = excelSheet.getRow(i);
-			if (fieldData != null) {
-				String fieldName = "";
-				String fieldType = "";
-				String fieldValue = "";
-				for (Cell cell : fieldData) {
-					int col = cell.getColumnIndex(); 
-					if (col == 0){
-						fieldName = cell.getStringCellValue();
-					} else if (col == 1){
-						fieldType = cell.getStringCellValue();
-					} else if (col < count + 2){
-						fieldValue = cell.getStringCellValue();
-					}
+			if (fieldData == null)
+				continue;
+			String fieldName = "";
+			String fieldType = "";
+			String fieldValue = "";
+			for (Cell cell : fieldData) {
+				cell.setCellType(Cell.CELL_TYPE_STRING);
+				int col = cell.getColumnIndex(); 
+				if (col == 0){
+					fieldName = cell.getStringCellValue();
+				} else if (col == 1){
+					fieldType = cell.getStringCellValue();
+				} else if (col < count + 2){
+					fieldValue = cell.getStringCellValue();
+					//get record corresponding to column index
+					LoadableData testcase = testcaseData.get(col);
+					testcase.put(fieldName, fieldType, fieldValue);
 				}
 			}
 		}
+	}
+	
+	public ArrayList<LoadableData> getDataProvider(){
+		return (ArrayList<LoadableData>) testcaseData.values();
+	}
+
+	@Override
+	public String toString() {
+		StringBuffer result = new StringBuffer("Parse testcase data from \n");
+		result.append("File  : " + fPath);
+		result.append("\nSheet : " + sheetName);
+		for(int i: testcaseData.keySet()){
+			LoadableData testcase = testcaseData.get(i);
+			result.append("\nTestcase's fields at column " + i + ": ");
+			result.append(testcase.toString());
+		}
+		return result.toString();
 	}
 }
